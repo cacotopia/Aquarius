846c54854979a86863d8c46cae08adc67fc20dea
cacotopia <sinosie7en@gmail.com>
2016-05-02 17:00:07 +0800

    AngelApp

---
+++ AngelApp/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angelApp')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
